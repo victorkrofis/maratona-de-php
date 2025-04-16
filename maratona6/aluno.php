f3d733b08899ebe27f44ea135387981b8cde0f14
victorkrofis <victorsilvaa7002@gmail.com>
2025-04-16 11:45:15 -0300

    Add files via upload

---
+++ maratona6/aluno.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../maratona6//css//style.css" type="text/css">
+</head>
+<body>
+    <h1>Notas dos Alunos</h1>
+    <div class="a2"><a href="http://localhost/maratona6/carro.php">Modelos de carros</a>
+    <a href="http://localhost/maratona6/pe%c3%a7as.php">Peças de carros</a>
+    </div>
+
+
+<form action="" method="POST">
+    <div>
+        <label for="nota">Nota:</label>
+        <input type="text" name="nota">
+    </div>
+    <input type="submit" value="Enviar">
+</form>
+
+<?php 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (!isset($_POST["nota"]) || !is_numeric($_POST["nota"])) {
+        echo "Por favor, insira um valor numérico válido.";
+    } else {
+        $nota = floatval($_POST["nota"]);
+
+        if ($nota < 0 || $nota > 10) {
+            echo "A nota deve estar entre 0 e 10.";
+        } elseif ($nota <= 4.9) {
+            echo "Reprovado";
+        } elseif ($nota >= 5 && $nota <= 6.9) {
+            echo "Recuperação";
+        } elseif ($nota >= 7 && $nota <= 8.9) {
+            echo "Aprovado";
+        } else {
+            echo "Aprovado com distinção";
+        }
+    }
+}
+?>
+</body>
+</html>
